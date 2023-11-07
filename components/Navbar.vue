f5857a19bfa31c48381608b5cf14748bbb748ca3
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-11-07 13:19:13 +0700

    Make Navbar for Desktop

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,7 +5,8 @@
             <div>
                 <img class="w-[3.5rem]" src="/img/honkasae_logo.png" alt="Logo" />
             </div>
-            <div class="mt-2">
+
+            <div class="mt-2 lg:flex">
                 <!-- Hamburger mobile menu -->
                 <NuxtLink class="lg:hidden grid gap-1 mt-1 bg-black/10 p-2 rounded-md" @click="toggleMenu">
                     <span v-bind:class="{ 'rotate-45': isToggleMenu, 'translate-y-[9px]': isToggleMenu }" id="ham1"
@@ -15,6 +16,14 @@
                     <span v-bind:class="{ '-rotate-45': isToggleMenu, '-translate-y-[7px]': isToggleMenu }" id="ham3"
                         class="w-6 h-1 bg-white transition-all duration-200"></span>
                 </NuxtLink>
+
+                <!--Menu Desktop-->
+                <div class="space-x-2 text-white text-[16px] font-bold hidden lg:flex lg:items-center">
+                    <button @click="atPages1" class="bg-black px-3 py-1 rounded-[20px]" :class="{'bg-white/50': atPages == 1}">ไม่มีอะไรแล้ว</button>
+                    <button @click="atPages2" class="bg-black px-3 py-1 rounded-[20px]" :class="{'bg-white/50': atPages == 2}">มันมีแค่ข่าวเดียวอะ</button>
+                    <button @click="atPages3" class="bg-black px-3 py-1 rounded-[20px]" :class="{'bg-white/50': atPages == 3}">ขี้เกียจทำต่อ</button>
+                    <button @click="atPages4" class="bg-black px-3 py-1 rounded-[20px]" :class="{'bg-white/50': atPages == 4}">พอแค่นี้แหละ</button>
+                </div>
             </div>
         </div>
 
@@ -35,12 +44,25 @@ export default {
     data() {
         return {
             isToggleMenu: false,
+            atPages: 1,
         };
     },
     methods: {
         toggleMenu() {
             this.isToggleMenu = !this.isToggleMenu;
         },
+        atPages1() {
+            this.atPages = 1;
+        },
+        atPages2() {
+            this.atPages = 2;
+        },
+        atPages3() {
+            this.atPages = 3;
+        },
+        atPages4() {
+            this.atPages = 4;
+        }
     },
 };
 </script>
